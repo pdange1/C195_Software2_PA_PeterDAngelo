d3d59f6a144890e6425ed674ba5ad91567ff95e3
Peter D'Angelo <pdange1@wgu.edu>
2023-04-19 12:21:00 -0700

    Structured the project according to the most recent DAO pattern video (9/15/2021).

--- src/sample/DAO/JDBC.java
+++ src/sample/DAO/JDBC.java
@@ -1,4 +1,4 @@
-package sample;
+package sample.DAO;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -5,12 +5,13 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import sample.DAO.JDBC;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/sample/view/sample.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
